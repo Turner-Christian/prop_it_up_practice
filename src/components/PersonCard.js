40a6b533f400b2f8295568606194abd86b4bacdb
Christian Turner <keycoderk@gmail.com>
2023-10-10 15:48:24 -0400

    Putting it together practice

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const PersonCard = (props) => {
     const { firstName, lastName, age, hairColor} = props;
+    const [ userAge, setUserAge ] = useState(parseInt(age));
 
     return (
         <div>
             <h1>{firstName}, {lastName}</h1>
-            <p>Age: {age}</p>
+            <p>Age: {userAge}</p>
             <p>Hair Color: {hairColor}</p>
+            <button onClick={() => setUserAge(userAge + 1)}>Birthday Button</button>
         </div>
     )
 }
