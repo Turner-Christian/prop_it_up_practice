df1a2df61cfb052a14ec6a9f7fee1fc3cea96750
Christian Turner <keycoderk@gmail.com>
2023-10-10 15:18:51 -0400

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import PersonCard from './components/PersonCard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PersonCard firstName={'Christian'} lastName={'Turner'}  age={'22'} hairColor={'Brown'} />
+      <PersonCard firstName={'Cat'} lastName={'Mur'}  age={'21'} hairColor={'Brown'} />
     </div>
   );
 }
